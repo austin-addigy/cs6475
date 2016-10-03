cd02bb7b087efed1acc7474998e83b39887156eb
Ram Subramanian <needmoarram@gmail.com>
2016-10-03 10:28:22 -0700

    a4 edges blah

--- assignment4/main.py
+++ assignment4/main.py
@@ -1,24 +1,7 @@
-
 import cv2
 import numpy as np
-import scipy as sp
-
-from os import path
 
 import assignment4 as a4
-
-"""
-You can use this file to write code for part 2. You are NOT required
-to use this file, and ARE ALLOWED to make ANY changes you want in
-THIS file. This file will not be submitted with your assignment
-or report, so if you write code for above & beyond effort, make sure
-that you include important snippets in your writeup. CODE ALONE IS
-NOT SUFFICIENT FOR ABOVE AND BEYOND CREDIT.
-
-    DO NOT SHARE CODE (INCLUDING TEST CASES) WITH OTHER STUDENTS.
-"""
-
-IMG_FOLDER = "test_images"
 
 GAUSSIAN_KERNEL = np.array([[  1,  4,  6,  4,  1],
                             [  4, 16, 24, 16,  4],
@@ -28,18 +11,27 @@ GAUSSIAN_KERNEL = np.array([[  1,  4,  6,  4,  1],
 
 
 def find_edges(image):
-    # TODO: implement your own edge detector
-    pass
-
+    
+    gx = a4.gradientX(image).astype(np.int64)
+    gx = np.hstack((gx, np.zeros((image.shape[0], 1))))
+    gy = a4.gradientY(image).astype(np.int64)
+    gy = np.vstack((gy, np.zeros((1, image.shape[1]))))
+    z = np.sqrt(gx**2 + gy**2)
+    z = cv2.GaussianBlur(z, (3, 3), 0.5)
+    
+    th = 30
+    z[z >= th] = 255
+    z[z < th] = 0
+    
+    return z
 
 def main():
-    # TODO: replace this code with your own
-
-    img = cv2.imread(path.join(IMG_FOLDER, "butterfly.jpg"), cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread("sauce.jpg", cv2.IMREAD_GRAYSCALE)
     edge_img = find_edges(img)
+    # edge_img = cv2.Canny(img, 100, 500)
     norm_img = a4.normalizeImage(edge_img)
-    cv2.imwrite("null_result.png", norm_img)
-
+    cv2.imwrite("edges.png", norm_img)
+    cv2.imwrite("orig.png", img)
 
 if __name__ == "__main__":
     main()
